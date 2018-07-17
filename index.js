c43710ccf931f8b5500aad650faab54a3c137789
tomtam6@gmail.com <tomtam6@gmail.com>
2018-07-17 20:12:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function logWhisper(string){
 
 function sayHiToGrandma(string){
   if (string === string.toLowerCase())  {
-    return "I can't hear you";
+    return "I can\'t hear you";
   }
 
 }
