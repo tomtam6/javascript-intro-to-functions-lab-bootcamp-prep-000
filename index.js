e002f340a329e04e4d57ed8e9b1122448d2950c1
tomtam6@gmail.com <tomtam6@gmail.com>
2018-07-17 20:17:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,5 +21,7 @@ function sayHiToGrandma(string){
     return "I can\'t hear you!";
   } else if (string === string.toUpperCase()) {
     return "YES INDEED!";
+  } else {
+    return "I love you, too.";
   }
 }
